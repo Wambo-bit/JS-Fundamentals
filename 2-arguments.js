63c36ee11d93f3604ecd915a19570ead578768dc
Mary <wambuimuthima1@gmail.com>
2025-09-14 19:15:02 +0300

    Arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -6,7 +6,7 @@ console.log("No argument");
 }
 else if(args.length ===1)
 {
-console.log("Argument Found");
+console.log("Argument found");
 }
 else
-console.log("Arguments Found");
+console.log("Arguments found");
