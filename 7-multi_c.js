3ea36ccab938279698791b530994f6cd36122db7
Mary <wambuimuthima1@gmail.com>
2025-09-14 19:31:37 +0300

    A multi_C.js

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+// Get the first argument
+const x = parseInt(process.argv[2], 10);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
