f79f159ea083073c3493ed3aef7d4f45b7e0451e
Mary <wambuimuthima1@gmail.com>
2025-09-14 19:23:30 +0300

    Create  a sentence

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the two arguments (ignoring "node" and script path)
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print them in the required format
+console.log(`${arg1} is ${arg2}`);
