14a9dae5e0f5e77aef008ca903e9701115efa099
Mary <wambuimuthima1@gmail.com>
2025-09-14 19:29:22 +0300

    Loop to Languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through and print each element
+for (const line of languages) {
+  console.log(line);
+}
